dd60165e707bf1ae0432ae23ab858e992597e7fa
Dylan Theno <djtheno@gmail.com>
2019-05-06 20:51:33 -0500

    breweries

--- client/src/App.js
+++ client/src/App.js
@@ -1,58 +1,54 @@
-import React, { Component } from 'react';
-import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
+import React, { Component } from 'react'
+import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react'
 
-import Navbar from './components/layout/Navbar';
-import Home from './components/Pages/Home';
-import Members from './components/Pages/Members';
-import Login from './components/auth/Login';
+import Navbar from './components/layout/Navbar'
+import Home from './components/Pages/Home'
+import Members from './components/Pages/Members'
+import Login from './components/auth/Login'
 
-import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Wine from './components/Pages/Wine/Wine'
 
 import Breweries from './components/Pages/Breweries/Breweries'
 // import MyNavbar from './components/MyNavbar'
-//import { isTemplateElement } from '@babel/types';
+// import { isTemplateElement } from '@babel/types';
 
-import './App.css';
+import './App.css'
 class App extends Component {
-  render() {
+  render () {
     return (
-     
       <Router>
-      <Security
-        issuer="https://dev-122012.okta.com/oauth2/default" 
-        client_id="0oahjmwthXxO2IB8f356"
-        redirect_uri={window.location.origin + '/implicit/callback'}
-        // onAuthRequired={onAuthRequired}
-      >
-      <div className="App">
+        <Security
+          issuer='https://dev-122012.okta.com/oauth2/default'
+          client_id='0oahjmwthXxO2IB8f356'
+          redirect_uri={window.location.origin + '/implicit/callback'}
+          // onAuthRequired={onAuthRequired}
+        >
+          <div className='App'>
             <Navbar />
-            <div className="container">
-              <Route path="/" exact={true} component={Home} />
-              <SecureRoute path="/Members" exact={true} component={Members} />
+            <div className='container'>
+              <Route path='/' exact component={Home} />
+              <SecureRoute path='/Members' exact component={Members} />
               <Route
-                path="/login"
+                path='/login'
                 render={() => (
-                  <Login baseUrl="https://dev-122012.oktapreview.com" />
+                  <Login baseUrl='https://dev-122012.oktapreview.com' />
                 )}
-              /> 
-              <Route exact path="/" component={Home} />
-             
-             <Route path="/wine" component={Wine} />
-          
-             <Route path="/Breweries" component={Breweries} />
-              <Route path="/implicit/callback" component={ImplicitCallback} />
+              />
+              <Route exact path='/' component={Home} />
+
+              <Route path='/wine' component={Wine} />
+
+              <Route path='/breweries' component={Breweries} />
+              <Route path='/implicit/callback' component={ImplicitCallback} />
             </div>
           </div>
         </Security>
       </Router>
-      
-    );
+    )
   }
-
-  
 }
 
-export default App;
+export default App
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,51 +1,46 @@
 import React, { Component } from 'react'
-import { Navbar, Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 // import './MyNavbar.css';
 
-
 export default class MyNavbar extends Component {
-  render() {
+  render () {
     return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
-        <div className="container">
-          <Link className="navbar-brand" to="/">
+      <nav className='navbar navbar-expand-sm navbar-dark bg-dark mb-4'>
+        <div className='container'>
+          <Link className='navbar-brand' to='/'>
             BoozIt Members Portal
           </Link>
           <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNav"
+            className='navbar-toggler'
+            type='button'
+            data-toggle='collapse'
+            data-target='#navbarNav'
           >
-            <span className="navbar-toggler-icon" />
+            <span className='navbar-toggler-icon' />
           </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/">
+          <div className='collapse navbar-collapse' id='navbarNav'>
+            <ul className='navbar-nav ml-auto'>
+              <li className='nav-item'>
+                <Link className='nav-link' to='/'>
                   Home
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/Members">
+              <li className='nav-item'>
+                <Link className='nav-link' to='/Members'>
                   Members
                 </Link>
               </li>
-              <li className="nav-item">
-              <Link className = "nav-link" to ="/beer">
-                Beer
-              </Link>
+
+              <li className='nav-item'>
+                <Link className='nav-link' to='/wine'>
+                  Beer
+                </Link>
               </li>
-              <li className ="nav-item">
-              <Link className = "nav-link" to = "/wine">
-                Wine
-              </Link>
-              </li>
-              <li className = "nav-item">
-              <Link className = "nav-link" to ="/liquor">
-                Liquor
-              </Link>
+              <li className='nav-item'>
+                <Link className='nav-link' to='/breweries'>
+                  Breweries
+                </Link>
               </li>
             </ul>
           </div>
@@ -72,6 +67,5 @@ export default class MyNavbar extends Component {
 //         )
 //     }
 // }
-
 
 // export default Navbar;
